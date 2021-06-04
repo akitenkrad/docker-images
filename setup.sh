ecb10c2946aaab6f2a0c41fc01b61dd41ac4c791
akitenkrad <akitenkrad@gmail.com>
2021-06-04 23:13:53 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -64,3 +64,8 @@ jupyter serverextension enable --py jupyterlab
 pip install jupyterlab_vim
 pip install jupyterlab-lsp
 pip install python-language-server[all]
+
+# install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
